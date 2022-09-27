cfc376650ef5e561ee71cfd0b0871cc56a4fa941
rifatrakib <Rakib.1508@outlook.com>
2022-09-27 19:31:17 +0600

    add necessary CORS configuration

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import config
 
@@ -6,6 +7,18 @@ from .auth import routes as auth_routes
 
 app = FastAPI()
 app.include_router(auth_routes.router)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
